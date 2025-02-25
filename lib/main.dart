94dbb788e35745812eef94fecc02b32bb9de3e9a
User_JXRR <a.22308051281326@cbtis128.edu.mx>
2025-02-25 16:59:00 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,59 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MyImg());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MyImg extends StatelessWidget {
+  const MyImg({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: (Colors.deepPurple),
+          leading: Icon(Icons.theater_comedy_outlined, color: Colors.white),
+          title: const Text("DICE Corp",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+                color: Colors.white,
+              )),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.network(
+                'https://i.pinimg.com/736x/92/18/dd/9218dd278ab2bad13b9f302858bb6844.jpg',
+                height: 150,
+              ),
+              Text(
+                "Peluca de Payaso",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+              Image.network(
+                "https://m.media-amazon.com/images/I/61Oa63zHiqL.jpg",
+                height: 150,
+              ),
+              Text(
+                "Nariz Roja",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+              Image.network(
+                  "https://m.media-amazon.com/images/I/71L8u4vMrnL._AC_UF894,1000_QL80_.jpg",
+                  height: 150),
+              Text(
+                "Zapatos Grandes",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
+            ],
+          ),
         ),
       ),
     );
